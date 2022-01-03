1bd75da6b91b224676666091059f6a063b5722df
Zigi <@dotcom>
2022-01-03 10:20:06 +0300

    Here is work revolving around the contact page. I think it's taking shape nicely

---
+++ contact.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet"  href="css/all.min.css" />
+	<link rel="stylesheet"  href="css/fontawesome.min.css" />
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto|Poppins|Trirong|Rubik|Oswald">
+	<link rel="stylesheet" type="text/css" href="contact.css" />
+
+</head>
+<body>
+	 <nav class="navContainer flex">
+		<div id="logo" >
+			
+		</div>
+		<ul class="navlinks flex">
+			<li><a href="contact.php">FAQ</a></li>
+			<li><a href="about.html">ABOUT</a></li>
+			<li><a href="service.html">OUR SERVICES</a></li>
+			<li><a href="testimonial.html">TESTIMONIALS</a></li>
+			<li><a href="contact.php">CONTACT</a></li>
+		</ul>
+	 </nav>
+	
+	 <section id="contact">
+
+	 	<div class="contactContainer flex">
+	 		<div class="contactInfo">
+
+	 			<h2 class="contactInfoTitle">Contact Information</h2>
+	 			<p>Comments? Questions? Inquiries? Reach us now. We are always glad to assist you. </p>
+
+	 			<div class="contactDetails flex">
+	 				<i class="fa fa-phone" aria-hidden="true"></i>
+	 				<span>+254-701-234567</span>
+	 			</div>
+
+	 			<div class="contactDetails flex">
+	 				<i class="fa fa-envelope" aria-hidden="true"></i>
+	 				<span>info@caselliexperts.com</span>
+	 			</div>
+
+	 			<div class="contactDetails flex">
+	 				<i class="fa fa-map-marker" aria-hidden="true"></i>
+	 				<span class="locationText">1st Floor, Suite A104, Madonna House Annex,Westlands Road.</span>
+	 			</div>
+
+	 			<div class="line">
+	 				
+	 			</div>
+	 			
+	 		</div>
+
+	 		<div class="contactForm">
+	 			
+	 		</div>
+	 	</div>
+	 	
+	 </section>
+
+	<section id="footer" class="flex">
+		<span> © 2022 Caselli Painting Experts LTD. All rights reserved.</span>
+	</section>
+
+</body>
+</html>
