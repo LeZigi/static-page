d3846f82b7c7e41d4156f5a85d4a2478f672768d
Zigi <@dotcom>
2022-01-23 14:28:59 +0300

    Major responsive changes and resources added

---
+++ fizbuzz.js
@@ -0,0 +1,14 @@
+for(let i=1; i<=100; i++){
+
+	if (i/3%0) {
+		console.log('Fizz')
+	}else if (i/5%0) {
+		console.log('buzz')
+
+	}else if (i/3%0&&i/5%0) {
+		console.log('fizzbuzz')
+	}else{
+		console.log(i)
+	}
+
+}
